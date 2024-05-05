c294689aa3b7fe4527b22a01569ad320d24ab5be
s117507 <s117507@ap.be>
2024-05-05 00:42:56 +0000

    updated

--- EJS/Testing/index.ts
+++ EJS/Testing/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import path from "path";
 import * as data from './card.json';
+import { Cards, GuildAfiliation } from "./interfaces";
 
 dotenv.config();
 
@@ -18,21 +19,34 @@ app.set("port", process.env.PORT ?? 3000);
 app.get("/", (req, res) => {
     const html = `
     <header class="navbar">
-    <nav>
-    <ul>
-        <li><a href="#">Speel</a></li>
-        <li><a href="#">Overzicht</a></li>
-        <li><a href="#">TeamPlanner</a></li>
-        <li><a href="#">Uitloggen</a></li>
-    </ul>
-    </nav>
+        <img src="assets/478d8e49233e7de64a3cbb875e526807.png" alt="Logo">
+        <nav>
+            <ul>
+                <li><a href="guesspokemon.html">Speel</a></li>
+                <li><a href="overzicht.html">Overzicht</a></li>
+                <li><a href="teamplanner.html">TeamPlanner</a></li>
+                <li><a href="index.html">Uitloggen</a></li>
+            </ul>
+        </nav>
     </header>`;
-    
+
     res.render("index", {
         title: "Card Game",
-        navbar: html
-    })
+        navbar: html 
+    });
 });
+
+app.get("/", (req: Request, res: Response) => {
+    try {
+        const cards: Cards[] = data; // Using the imported JSON data
+        res.render('index', { cards }); // Passing the 'cards' variable to the EJS template
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
 
 app.listen(app.get("port"), () => {
     console.log("Server started on http://localhost:" + app.get("port"));
---
+++ EJS/Testing/interfaces.ts
@@ -0,0 +1,20 @@
+export interface Cards {
+id: string;
+name: string;
+description: string;
+rating: number;
+isActive: boolean;
+birthDate: string;
+imageUrl: string;
+type: string;
+abilities: string[];
+artist: string;
+guildAffiliation: GuildAfiliation;
+}
+
+export interface GuildAfiliation {
+    id: string;
+    name: string;
+    country: string;
+}
+
