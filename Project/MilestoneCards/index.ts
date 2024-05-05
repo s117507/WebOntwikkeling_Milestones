4e0c2930a9cd676034e153f85022e680c10856f6
s117507 <s117507@ap.be>
2024-05-05 21:16:41 +0000

    updated

--- Project/MilestoneCards/index.ts
+++ Project/MilestoneCards/index.ts
@@ -1,9 +1,9 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import path from "path";
-import { default as fetch } from 'node-fetch';
 import { Cards } from "./interfaces";
 import { MongoClient, Collection } from 'mongodb';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -22,24 +22,6 @@ let dbCollection: Collection<Cards>;
 const uri = process.env.MONGODB_URI || 'mongodb+srv://estalistrinev:tPqvaqEIdP7z9KM1@mijnproject.udzcq5y.mongodb.net/';
 const client = new MongoClient(uri);
 
-async function fetchDataAndWriteToMongoDB() {
-    try {
-        let cardsData = await fetch('https://raw.githubusercontent.com/s117507/WebOntwikkeling_Milestones/main/Project/MilestoneCards/card.json');
-        let jsonData = await cardsData.json();
-        let cards: Cards[] = jsonData as Cards[];
-
-        const count = await dbCollection.countDocuments();
-        if (count === 0) {
-            await dbCollection.insertMany(cards);
-            console.log('Data inserted into MongoDB');
-        } else {
-            console.log('Data already exists in MongoDB');
-        }
-    } catch (error) {
-        console.error('Error fetching data and writing to MongoDB:', error);
-    }
-}
-
 async function startServer() {
     try {
         await app.listen(app.get("port"));
@@ -49,6 +31,24 @@ async function startServer() {
     }
 }
 
+async function insertCardsIntoDB() {
+    try {
+        const count = await dbCollection.countDocuments();
+        if (count === 0) {
+            const response = await fetch('https://raw.githubusercontent.com/s117507/WebOntwikkeling_Milestones/main/Project/MilestoneCards/card.json');
+            const jsonData = await response.json();
+            const cards: Cards[] = jsonData as Cards[];
+
+            console.log('Data inserted into MongoDB');
+        } else {
+            console.log('Data already exists in MongoDB');
+        }
+    } catch (error) {
+        console.error('Error inserting data into MongoDB:', error);
+    }
+}
+
+
 (async () => {
     try {
         await client.connect();
@@ -57,7 +57,7 @@ async function startServer() {
 
         console.log('Connected to MongoDB');
 
-        await fetchDataAndWriteToMongoDB();
+        await insertCardsIntoDB();
         await startServer();
     } catch (error) {
         console.error('Error:', error);
@@ -68,7 +68,7 @@ app.get("/", async (req, res) => {
     try {
         const { search, sortField, sortDirection } = req.query;
 
-        let filteredCards: Cards[] = await dbCollection.find({}).toArray();
+        let filteredCards = await dbCollection.find({}).toArray();
 
         if (search) {
             filteredCards = filteredCards.filter((card: Cards) => card.name.toLowerCase().includes(search.toString().toLowerCase()));
@@ -126,7 +126,7 @@ app.post('/edit/:name', async (req, res) => {
             name,
             description,
             type,
-            rating: parseInt(rating) 
+            rating: parseInt(rating) // Assuming rating is a number
         };
 
         const result = await dbCollection.updateOne(
